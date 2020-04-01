ea00a904b29d6ea6f9d57fe73a0a733dbc46b491
PeteSampras <Petesampras.mmobugs@gmail.com>
2020-03-31 15:53:30 -1000

    Updates

---
+++ precache-manifest.5bad80e6ecc81080daf04614f53d69b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26234b90b3f3e9b107011d2a5af8f7a",
+    "url": "/mitre-attck-type-react-demo/index.html"
+  },
+  {
+    "revision": "113daf72eace0166e38d",
+    "url": "/mitre-attck-type-react-demo/static/css/main.38b223d9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mitre-attck-type-react-demo/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mitre-attck-type-react-demo/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113daf72eace0166e38d",
+    "url": "/mitre-attck-type-react-demo/static/js/main.e120f403.chunk.js"
+  },
+  {
+    "revision": "5c2224d09fa798008517",
+    "url": "/mitre-attck-type-react-demo/static/js/runtime-main.8a04388c.js"
+  }
+]);
